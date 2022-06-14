811ea2c59e585847fc8e7c9414d16fa8c82f9174
th0r0nd0r <pjfritch1@gmail.com>
2022-06-14 16:37:08 +0800

    configuring app

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const cors = require("cors");
 // const AppError = require("./utils/appError");
 // const errorHandler = require("./utils/errorHandler");
 
+// initialize app + set server port
+const app = express();
+const server = {port: 3000};
+
+// apply middleware (cors for CORS config and express.json for parsing requests
+app.use(cors());
+app.use(express.json());
+
