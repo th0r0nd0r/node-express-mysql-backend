8004bbdb20b0454198143bedf5eed2438f5b3bb8
th0r0nd0r <pjfritch1@gmail.com>
2022-06-24 11:23:42 +0800

    configure prettier

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // using import instead of require by setting "type": "module" in package.json
 import express from 'express';
 import cors from 'cors';
+import database from './setup/database';
 // const router = require("./routes");
 // const AppError = require("./utils/appError");
 // const errorHandler = require("./utils/errorHandler");
@@ -9,7 +10,7 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT;
 
-// apply middleware 
+// apply middleware
 
 // CORS config
 app.use(cors());
@@ -18,27 +19,19 @@ app.use(express.json());
 // sets the default dir from which to serve static assets
 app.use(express.static('public'));
 
-
-
 // defining basic routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-// start the server
-app.listen( port , () => console.log(`Server started, listening port: ${port}`));
-
-
-
-
-
-
-
-
-
-
-
-
-
+// connect to db + start the server
+database
+  .connect()
+  .then(() =>
+    app.listen(port, () =>
+      console.log(`Server started, listening port: ${port}`)
+    )
+  )
+  .catch((err) => console.log(err));
 
 // TODO: cache static files for production https://expressjs.com/en/advanced/best-practice-performance.html#use-a-reverse-proxy
---
+++ setup/database.js
@@ -0,0 +1,18 @@
+import { Sequelize } from 'sequelize';
+
+const {
+  PORT: port,
+  HOST: host,
+  DATABASE_DIALECT: dialect,
+  DATABASE_NAME: database_name,
+  MSQL_USERNAME: username,
+  MYSQL_PASSWORD: password,
+} = process.env;
+
+export default {
+  connect: () => new Sequelize(database_name, username, password, {
+    port,
+    host,
+    dialect,
+  }),
+}
