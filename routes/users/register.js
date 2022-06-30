a0e390364f0bbbdc017ec5fcf3ecc1f77b2444a0
th0r0nd0r <pjfritch1@gmail.com>
2022-06-30 20:40:01 +0800

    add schemas and handle cases for registrations

--- routes/users/register.js
+++ routes/users/register.js
@@ -1,4 +1,6 @@
 import Joi from 'joi';
+import UserModel from '../../models/User.js';
+import { Op } from 'sequelize';
 import { Router } from 'express';
 const router = Router();
 
@@ -11,6 +13,7 @@ router.post('/', (req, res, next) => {
     username: Joi.string().max(15).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(8).max(60).required(),
+    confirmPassword: Joi.string().min(8).max(60).required(),
   });
 
   const respSchema = Joi.object({
@@ -23,6 +26,28 @@ router.post('/', (req, res, next) => {
   if (reqError) {
     return res.status(400).json(reqError.message);
   }
+  
+  const { username, email, password, confirmPassword } = body;
 
+  UserModel.findOne({ where: { [Op.or]: [{ email, username }] } }).then(
+    (user) => {
 
+      if (user) {
+        // TODO:
+        // if (user.email === body.email && hash(body.password) !== user.password)
+        //    return res.status(400).json('Incorrect email and password combination');
+        // else
+        //    redirect to login page
+
+        if (username === user.username) {
+          return res.status(409).json('Username already exists');
+        }
+      } else {
+
+        if (password !== confirmPassword) {
+          return res.status(400).json('Passwords do not match');
+        }
+      }
+    }
+  );
 });
